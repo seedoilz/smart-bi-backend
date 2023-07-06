4537b64380359bde1a156d49342f945323896276
seedoilz <seedoilz609@qq.com>
2023-07-06 17:27:45 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "----------maven clean and install-----------"
-mvn clean
+#mvn clean
 mvn install -Dmaven.test.skip=true
-java -jar target/smart_bi-0.0.1-SNAPSHOT.jar
+BUILD_ID=dontKillMe nohup java -jar target/smart_bi-0.0.1-SNAPSHOT.jar &
